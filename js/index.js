e8bea560d01170a8d4f87c04c833a21687f174b1
Juan De Oliveira <juanabrahamd@gmail.com>
2023-01-24 11:55:13 +0100

    Btns done

--- js/index.js
+++ js/index.js
@@ -24,8 +24,8 @@ pauseBtn.addEventListener('click', () => {
         document.getElementById('play-btn').style.visibility = 'visible';
     } else {
         game.start()
-        document.getElementById('pause-btn').style.visibility = 'hidden';
-        document.getElementById('play-btn').style.visibility = 'visible';
+        document.getElementById('pause-btn').style.visibility = 'visible';
+        document.getElementById('play-btn').style.visibility = 'hidden';
     }
 })
 
@@ -36,7 +36,7 @@ resetBtn.addEventListener('click', () => {
 })
 
 function changeBtns() {
-    document.getElementById('play-btn').style.visibility = 'visible';
+    document.getElementById('play-btn').style.visibility = 'hidden';
     document.getElementById('pause-btn').style.visibility = 'visible';
     document.getElementById('reset-btn').style.visibility = 'visible';
     document.getElementById('intro').style.visibility = 'hidden';
